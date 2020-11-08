f526f8a3fda5a27e768e2763b62bfe35d03bdc05
joshua-augustinus <joshua.augustinus@gmail.com>
2020-11-09 07:30:01 +1100

    First commit

---
+++ src/screens/MasterScreen.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react';
+import { Button, Text, TextInput, TouchableOpacity, View, BackHandler } from 'react-native';
+import { SafeAreaView, StackActions } from 'react-navigation';
+import { DrawerActions, NavigationDrawerProp } from 'react-navigation-drawer';
+
+/**
+ * https://reactnavigation.org/docs/4.x/typescript
+ */
+type Props = {
+    navigation: NavigationDrawerProp<{ userId: string, routeName: string }>;
+}
+
+const MasterScreen = (props: Props) => {
+
+    useEffect(() => {
+        BackHandler.addEventListener('hardwareBackPress', function () {
+            console.log("Back called");
+            return true;
+        });
+    }, []);
+
+    const onMenuPress = () => {
+        console.log(props.navigation.state);// { key: 'Home', routeName: 'Home' }
+        console.log("Menu pressed");
+        props.navigation.dispatch(DrawerActions.toggleDrawer());
+    }
+
+    const onButtonPress = () => {
+        const pushAction = StackActions.push({
+            routeName: 'Stack1',
+            params: {
+                myUserId: 9,
+            },
+        });
+
+        props.navigation.dispatch(pushAction);
+    }
+
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ height: 50, backgroundColor: 'red', flexDirection: 'row', alignItems: 'center' }}>
+
+                <TouchableOpacity style={{ backgroundColor: 'yellow' }}
+                    onPress={() => onMenuPress()}>
+                    <Text>Menu</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+                <Text>{props.navigation.state.routeName}</Text>
+                <TextInput placeholder="Enter text here..."></TextInput>
+                <Button title="Press me" onPress={() => onButtonPress()}></Button>
+            </View>
+        </SafeAreaView>
+
+    );
+
+}
+
+MasterScreen.navigationOptions = {}
+
+export { MasterScreen }
